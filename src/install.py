3db8cf70c438293821002c86d8fe3d0e513a64c1
Judah Fuller <judahfuller@gmail.com>
2023-04-21 14:02:51 +0100

    Add support for movable binary files

--- src/install.py
+++ src/install.py
@@ -9,9 +9,13 @@ def addToInstallList(package, location):
 def install(package: Package, settings=Settings()):
         package.download()
         print("downloaded")
-        match package.FileType:
-                case "py": exec(open("cache/" + package.Identifier + "." + package.FileType).read())
-                case "sh":subprocess.call(["sh", "cache/" + package.Identifier + "." + package.FileType])
+        if package.Type == "DirectRun":
+                match package.FileType:
+                        case "py": exec(open("cache/" + package.Identifier + "." + package.FileType).read())
+                        case "sh":subprocess.call(["sh", "cache/" + package.Identifier + "." + package.FileType])
+        elif package.Type == "executable":
+                subprocess.call(["mv", "cache/" + package.Identifier + "." + package.FileType, "bin/"])
+                subprocess.call(["chmod", "755", "bin/" + package.Identifier + "." + package.FileType])
         if settings.clearCache == True: 
                 subprocess.call(["rm", "-r", "cache/"])
                 subprocess.call(["mkdir", "cache/"])
--- src/main.py
+++ src/main.py
@@ -1,15 +1,12 @@
 from random import randint
 from asciimatics.screen import Screen
+from time import sleep
 
-def demo(screen):
+def demo(screen: Screen):
+    screen.print_at('Zen Br', 0, 0)
     while True:
-        screen.print_at('Hello world!',
-                        randint(0, screen.width), randint(0, screen.height),
-                        colour=randint(0, screen.colours - 1),
-                        bg=randint(0, screen.colours - 1))
         ev = screen.get_key()
         if ev in (ord('Q'), ord('q')):
             return
         screen.refresh()
-
 Screen.wrapper(demo)

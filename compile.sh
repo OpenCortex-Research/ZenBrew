e2d426c1d6084f0943a4ae4fabd42bdca8804197
Judah Fuller <judahfuller@gmail.com>
2023-04-23 21:30:05 +0100

    Delete FIles

--- compile.sh
+++
@@ -1,1 +0,0 @@
-python3 -m nuitka --follow-imports --onefile src/main.py
--- prep-compile.sh
+++
@@ -1,3 +0,0 @@
-apt install zlib1g zlib1g-dev python3 python-pip3 patchelf
-pip3 install cmake nuitka zstandard
-pip3 install scikit-build python-Levenshtein fuzzywuzzy fuzzywuzzy[speedup]

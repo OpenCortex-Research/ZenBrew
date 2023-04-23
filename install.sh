240ac34206efd1e58700c8b7f49d534042832c3a
Judah Fuller <judahfuller@gmail.com>
2023-04-23 21:31:04 +0100

    Add Link

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ sudo apt install wget python3 python3-pip -y -qq
 pip3 install python-Levenshtein fuzzywuzzy fuzzywuzzy[speedup]
 mkdir /opt/OpenCortex/
 cd /opt/OpenCortex/
-wget ""
+wget "https://github.com/JudahZF/ZenBrew/archive/refs/heads/main.zip"
 unzip ZenBrew.zip
 cd ZenBrew
 sudo ln -s src/main.py /bin/ZenBrew

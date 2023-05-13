aa553ac6def258632c89bfd74740b4f64e60400f
Judah Fuller <judahfuller@icloud.com>
2023-05-13 11:09:38 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,5 @@ gunzip ZenBrew-0_1_2.tar.gz
 tar -xf ZenBrew-0_1_2.tar
 rm ZenBrew-0_1_2.tar
 ln -s /opt/OpenCortex/ZenBrew/main.py /bin/zenbrew
-ln -s /opt/OpenCortex/ZenBrew/main.py /usr/bin/zenbrew
 chmod +x /opt/OpenCortex/ZenBrew/main.py
 chmod +x /bin/zenbrew
-chmod +x /usr/bin/zenbrew

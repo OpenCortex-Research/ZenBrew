7b14ab56fb4427b386e30ce5343821bbda7e0d24
Judah Fuller <judahfuller@icloud.com>
2023-05-13 10:45:01 +0100

    Fix updater

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 mkdir /opt/OpenCortex/
 cd /opt/OpenCortex/
-curl "https://github.com/OpenCortex-Research/ZenBrew/releases/download/V0.1.0/ZenBrew-0_1_1.tar.gz" -o ZenBrew-0_1_1.tar.gz
-gunzip ZenBrew-0_1_1.tar.gz
-tar -xf ZenBrew-0_1_1.tar
-rm ZenBrew-0_1_1.tar
-ln -s ZenBrew/main.py /bin/ZenBrew
+curl "https://github.com/OpenCortex-Research/ZenBrew/releases/download/V0.1/ZenBrew-0_1_2.tar.gz" -o ZenBrew-0_1_2.tar.gz
+gunzip ZenBrew-0_1_2.tar.gz
+tar -xf ZenBrew-0_1_2.tar
+rm ZenBrew-0_1_2.tar
+ln -s /opt/OpenCortex/ZenBrew/main.py /bin/ZenBrew

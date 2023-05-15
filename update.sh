d9424b590605f905825b10dd1ac18f8af8c121e7
Judah Fuller <25085396+JudahZF@users.noreply.github.com>
2023-05-15 08:22:15 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,3 +1,4 @@
+#! /bin/sh
 mkdir /opt/OpenCortex/cache
 cd /opt/OpenCortex/cache
 curl -L https://github.com/OpenCortex-Research/ZenBrew/releases/download/V0.1.3/ZenBrew.tar.gz -o ZenBrew.tar.gz
@@ -6,8 +7,7 @@ tar -xf ZenBrew.tar
 rm ZenBrew.tar
 cp --recursive --preserve --update /opt/OpenCortex/cache/ZenBrew/ /opt/OpenCortex/
 rm -r /opt/OpenCortex/cache/
-rm /bin/zenbrew
-ln -s /opt/OpenCortex/ZenBrew/main.py /bin/zenbrew
+rm /usr/bin/zenbrew
+ln -s /opt/OpenCortex/ZenBrew/main.py /usr/bin/zenbrew
 chmod +x /opt/OpenCortex/ZenBrew/main.py
-chmod +x /bin/zenbrew
 chmod +x /usr/bin/zenbrew

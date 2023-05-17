b7a8b3627ea9c99dea98992a320c87bd8e90c07d
Judah Fuller <25085396+JudahZF@users.noreply.github.com>
2023-05-17 13:00:52 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 mkdir /media/p4/OpenCortex/
 cd /media/p4/OpenCortex/
-curl -L -s https://github.com/OpenCortex-Research/ZenBrew/archive/refs/tags/V0.2.1.tar.gz -o ZenBrew.tar.gz
+curl -L -s https://github.com/OpenCortex-Research/ZenBrew/releases/download/V0.2.1/ZenBrew.tar.gz -o ZenBrew.tar.gz
 gunzip ZenBrew.tar.gz
 tar -xf ZenBrew.tar
 rm ZenBrew.tar

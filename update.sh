54e61b8ed9b137d8efb90ab83d62698b50bb13bb
Judah Fuller <25085396+JudahZF@users.noreply.github.com>
2023-05-17 12:38:24 +0100

    Fix cp

--- update.sh
+++ update.sh
@@ -5,7 +5,7 @@ curl -L https://github.com/OpenCortex-Research/ZenBrew/releases/download/V0.2.0/
 gunzip ZenBrew.tar.gz
 tar -xf ZenBrew.tar
 rm ZenBrew.tar
-cp --recursive --preserve --update /media/p4/OpenCortex/cache/ZenBrew/ /media/p4/OpenCortex/
+cp -R -p -f /media/p4/OpenCortex/cache/ZenBrew/ /media/p4/OpenCortex/
 rm -r /media/p4/OpenCortex/cache/
 rm /usr/bin/zenbrew
 ln -s /media/p4/OpenCortex/ZenBrew/main.py /usr/bin/zenbrew

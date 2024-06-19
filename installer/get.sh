84987304a74cf1abdf366f60e938283c7eb9aee9
Judah Fuller <25085396+JudahZF@users.noreply.github.com>
2024-06-19 08:48:28 +0100

    Update get.sh

--- installer/get.sh
+++ installer/get.sh
@@ -5,8 +5,8 @@ mkdir -p /media/p4/OpenCortex/bin
 mkdir -p /media/p4/OpenCortex/ZenBrew
 cd /media/p4/OpenCortex/bin
 curl -O https://github.com/OpenCortex-Research/ZenBrew/releases/download/V1.0.0/zenbrew
-chmod +x /media/p4/OpenCortex/Bin/zenbrew
+chmod +x /media/p4/OpenCortex/bin/zenbrew
 cd /media/p4/OpenCortex/ZenBrew
 curl -O https://github.com/OpenCortex-Research/ZenBrew/releases/download/V1.0.0/settings.json
 cd /media/p4/OpenCortex/bin
-zenbrew install zenbrew
+./zenbrew install zenbrew
